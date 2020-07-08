61c97bcb1f630f7e1d5dcc92906af615c38d01f4
govindgoel <govindgoel82@gmail.com>
2020-07-09 03:54:28 +0530

    add function in views

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -21,19 +21,12 @@ def upload(request):
         name = fs.save(uploaded_file.name, uploaded_file)
         context['url'] = fs.url(name)
         file_path = os.path.join('./media', 'input.txt') # full path to text.
-        data_file = open(file_path , 'rb')   #I used'rb' here since I got an 'gbk' decode error
-        data = data_file.read()
-        file1 = open(file_path,"r+")
-        print ("Output of Read function is ")
-        print (file1.read())
-        print
-        print("data=", data)
     return render(request, 'upload.html', context)
 
 def fun1(request):
     context = {}
     if request.method == 'POST':
-        shutil.copyfile('./media/input.txt', './media/fun1.txt')
+        shutil.copyfile('./media/inputfun1.txt', './media/fun1.txt')
         fs = FileSystemStorage()
         context['url'] = fs.url('./fun1.txt')
         file_path = os.path.join('./media','fun1.txt')
@@ -55,13 +48,15 @@ def fun2(request):
     #pattern matching
     context = {}
     if request.method == 'POST':
-        shutil.copyfile('./media/input.txt', './media/fun2.txt')
+        shutil.copyfile('./media/inputfun2.txt', './media/fun2.txt')
         fs = FileSystemStorage()
         context['url'] = fs.url('./fun2.txt')
         file_path = os.path.join('./media','fun2.txt') # full path to text.
         file1 = open(file_path,"r+")
         Text = file1.readlines()
-        PatternMatching(Text[0], Text[1], file1)
+        print ("Output of Read function is ")
+        print(Text)
+        PatternMatching(Text[0],Text[1],file1)
     return render(request, 'fun2.html', context)
 
 def PatternMatching(Text, Pattern,file1):
@@ -71,13 +66,15 @@ def PatternMatching(Text, Pattern,file1):
             if Text[j] == Pattern[j-i]:
                 flag+=1
         if flag == len(Pattern):
-            file1.write(str(i))
+            cnum = str(i)
+            file1.write(cnum)
+            print(cnum)
 
 def fun3(request):
     #Frequent words
     context = {}
     if request.method == 'POST':
-        shutil.copyfile('./media/input.txt', './media/fun3.txt')
+        shutil.copyfile('./media/inputfun3.txt', './media/fun3.txt')
         fs = FileSystemStorage()
         context['url'] = fs.url('./fun3.txt')
         file_path = os.path.join('./media','fun3.txt') # full path to text.
@@ -90,17 +87,23 @@ def FrequentWords(Text, k, file1):
     count = {}
     FrequentPattern_raw = []
     FrequentPattern = []
-    for i in range(len(Text)-k+1):
-        Pattern = Text[i:i+k]
+    z = len(Text)
+    z1 = int(z)
+    x = int(k)
+    for i in range(z1-x+1):
+        Pattern = Text[i:i+x]
         count[Pattern] = PCount(Text, Pattern)
     maxCount = max(count.values())
-    for i in range(len(Text)-k+1):
-        if count[Text[i:i+k]] == maxCount:
-            FrequentPattern_raw.append(Text[i:i+k])
+    for i in range(z1-x+1):
+        if count[Text[i:i+x]] == maxCount:
+            FrequentPattern_raw.append(Text[i:i+x])
+            file1.write(Text[i:i+x])
     for i in FrequentPattern_raw:
         if i not in FrequentPattern:
             FrequentPattern.append(i)
-    file1.write(FrequentPattern)
+            file1.write(i)
+            print(i)
+    
 
 def PCount(Text, Pattern):
     count = 0
@@ -113,7 +116,7 @@ def fun4(request):
     #Reverse
     context = {}
     if request.method == 'POST':
-        shutil.copyfile('./media/input.txt', './media/fun4.txt')
+        shutil.copyfile('./media/inputfun4.txt', './media/fun4.txt')
         fs = FileSystemStorage()
         context['url'] = fs.url('./fun4.txt')
         file_path = os.path.join('./media','fun4.txt') # full path to text.
@@ -142,7 +145,7 @@ def fun5(request):
     #clump
     context = {}
     if request.method == 'POST':
-        shutil.copyfile('./media/input.txt', './media/fun5.txt')
+        shutil.copyfile('./media/inputfun5.txt', './media/fun5.txt')
         fs = FileSystemStorage()
         context['url'] = fs.url('./fun5.txt')
         file_path = os.path.join('./media','fun5.txt') # full path to text.
@@ -153,20 +156,23 @@ def fun5(request):
 
 def ClumpFinding(Genome, k, L,t, file1):
     j = 0
+    L1  = int(L)
+    t1  = int(t)
+    k1  = int(k)
     clump = set()
-    while (j+L)<=len(Genome):
+    while (j+L1)<=len(Genome):
         substrate = {}
-        w = Genome[j:j+L]
+        w = Genome[j:j+L1]
         f = 0
-        while (f+k)<=len(w):
-            sub = w[f:f+k]
+        while (f+k1)<=len(w):
+            sub = w[f:f+k1]
             if sub not in substrate:
                 substrate[sub] = 1
             else :
                 substrate[sub]+=1
             f+=1
         j+=1
-        clumpList = [key for key in substrate.keys() if substrate[key]>=t]
+        clumpList = [key for key in substrate.keys() if substrate[key]>=t1]
         for l in clumpList:
             clump.add(l)
     file1.write(str(len(clump)))
@@ -175,7 +181,7 @@ def fun6(request):
     #skew
     context = {}
     if request.method == 'POST':
-        shutil.copyfile('./media/input.txt', './media/fun6.txt')
+        shutil.copyfile('./media/inputfun6.txt', './media/fun6.txt')
         fs = FileSystemStorage()
         context['url'] = fs.url('./fun6.txt')
         file_path = os.path.join('./media','fun6.txt') # full path to text.
@@ -191,17 +197,22 @@ def Skew(Pattern, file1):
     for i in range(len(Pattern)):
         if Pattern[i] == 'C':
             skewArray[i+1] = skewArray[i]-1
+            c = skewArray[i+1]
+            file1.write(str(c))
         elif  Pattern[i] == 'G':
             skewArray[i+1] = skewArray[i]+1
+            c1 = skewArray[i+1]
+            file1.write(str(c1))
         else:
             skewArray[i+1] = skewArray[i]
-    file1.write(skewArray)
+            c2 = skewArray[i+1]
+            file1.write(str(c2))
 
 def fun7(request):
     #mismatched
     context = {}
     if request.method == 'POST':
-        shutil.copyfile('./media/input.txt', './media/fun7.txt')
+        shutil.copyfile('./media/inputfun7.txt', './media/fun7.txt')
         fs = FileSystemStorage()
         context['url'] = fs.url('./fun7.txt')
         file_path = os.path.join('./media','fun7.txt') # full path to text.
@@ -210,26 +221,27 @@ def fun7(request):
         MFrequentWords(Text[0], Text[1], Text[2], file1)
     return render(request, 'fun7.html', context)
 
-def MFrequentWords(s,k,d, file1):
+def MFrequentWords(s,k,d,file1):
+    k1 = int(k)
+    d1 = int(d)
     counts = {}
-    for i in range(len(s)-k+1):
-        for neighbor in neighbors(s[i:i+k],d):
+    for i in range(len(s)-k1+1):
+        for neighbor in neighbors(s[i:i+k1],d1):
             if neighbor not in counts:
                 counts[neighbor] = 0
             counts[neighbor] += 1
     m = max(counts.values())
-    for kmer in counts: 
-        if counts[kmer] == m:
-            file1.write(kmer)
+    str1 = ''.join([kmer for kmer in counts if counts[kmer] == m])
+    file1.write(str1)
 
-def neighbors( s, d ):
-    if d == 0:
+def neighbors( s, d1 ):
+    if d1 == 0:
         return [s]
     if len(s) == 1:
         return ['A','C','G','T']
     out = []
-    for neighbor in neighbors(s[1:],d):
-        if hamm(s[1:],neighbor) < d:
+    for neighbor in neighbors(s[1:],d1):
+        if hamm(s[1:],neighbor) < d1:
             out.extend(['A'+neighbor,'C'+neighbor,'G'+neighbor,'T'+neighbor])
         else:
             out.append(s[0] + neighbor)
@@ -247,7 +259,7 @@ def fun8(request):
     #hamming
     context = {}
     if request.method == 'POST':
-        shutil.copyfile('./media/input.txt', './media/fun8.txt')
+        shutil.copyfile('./media/inputfun8.txt', './media/fun8.txt')
         fs = FileSystemStorage()
         context['url'] = fs.url('./fun8.txt')
         file_path = os.path.join('./media','fun8.txt') # full path to text.
